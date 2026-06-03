67ef52fd0820022d37db98b4de1ac07492c0d08e
Harshsurana-hub <harshsuran2006@gmail.com>
2026-06-03 09:35:03 +0530

    Add files via upload

---
+++ op.py
@@ -0,0 +1,52 @@
+# for i in range(51):
+#   if(i%2==0):
+#     print(i)
+# for i in range(51):
+#   if(i%2!=0):
+#     print(i)
+
+# a = 3
+# if a%2 == 0:
+#   print("even")
+# else:
+#   print("odd")
+    
+# a = 0
+# if a>0 :
+#   print("Positive")
+
+# elif a==0 :
+#     print("neutral")
+# else :
+#   print("negative")
+
+# Day = input("Enter Short Form: ")
+
+# if Day =='Mon':
+#   print("Monday")
+# elif Day == 'Tue':
+#   print("Tuesday")
+# elif Day == 'Wed':
+#   print("Wednesday")
+# elif Day == 'Thr':
+#   print("Thrusday")
+# elif Day == 'Fri':
+#   print("Friday")
+# elif Day == 'Sat':
+#   print("Saturday")
+# else :
+#   print("Sunday")
+
+# for i in range(start,end,increment) :
+#  print(i)
+
+# a= 1
+# while a<10:
+#   print(a)
+#   a=a+1
+
+a = input()
+s = len(a) 
+b = 0
+for i in range(s,0,-1):
+  print(a)
